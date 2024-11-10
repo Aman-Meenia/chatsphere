0a93da7c750cdaa007291787d586be3a69d900c4
Aman-Meenia <amanmeenia09876@gmail.com>
2024-11-10 23:10:38 +0530

    Add pusher key

--- src/pusher/pusher.ts
+++ src/pusher/pusher.ts
@@ -1,10 +1,11 @@
 import PusherServer from "pusher";
 import Pusher from "pusher-js";
+
 export const pusherServer = new PusherServer({
-  appId: process.env.PUSHER_APP_ID || "",
-  key: process.env.PUSHER_KEY || "",
-  secret: process.env.PUSHER_SECRET || "",
-  cluster: process.env.PUSHER_CLUSTER || "",
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.PUSHER_CLUSTER!,
   useTLS: true,
 });
 
